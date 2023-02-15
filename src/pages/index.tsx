363baa21c911b9b8c9c32a0eaa430fb6b3943dfb
Nicolas Cauet <nicolascauetwork@gmail.com>
2023-02-15 21:44:13 +0100

    improve page styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import Head from "next/head";
 import { useState } from "react";
 
 import Offers from "@/components/Offers";
-import OfferDetails from "@/components/OfferDetails";
 
 export default function Home() {
   const [isLoggedInUser, setIsLoggedInUser] = useState(true);
@@ -17,8 +16,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1 className="text-3xl font-bold underline">
+      <main className="max-w-7xl m-auto px-4">
+        <h1 className="text-5xl font-bold underline my-8">
           Discover the most liked offers on reMarket
         </h1>
         <input
@@ -27,8 +26,12 @@ export default function Home() {
           onChange={swithLoggingStatus}
           name="login"
           id="login"
+          className="mb-8 mr-3"
         />
-        <label htmlFor="login">logged in</label>
+        <label className="" htmlFor="login">
+          logged in (Simulate logged in user vs. visitor by toogling this
+          checkbox)
+        </label>
         <Offers isLoggedInUser={isLoggedInUser} />
       </main>
     </>
